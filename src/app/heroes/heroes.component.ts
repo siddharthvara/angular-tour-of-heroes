75217b214fe5515c9edb132816d6309d969a2407
siddharth.vara.jira <siddharth.vara@azilen.com>
2019-11-14 21:42:49 +0530

    call service inside component.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+//import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service'
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
@@ -14,16 +16,21 @@ export class HeroesComponent implements OnInit {
     name: 'siddharth'
   };
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
-  
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
